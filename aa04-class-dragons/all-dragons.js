7b64453101e71e217139920409ea6a7c1a0816e0
JamesAnderson30 <jamesanderson_18@hotmail.com>
2023-10-31 06:02:11 -0400

    finished

--- aa04-class-dragons/all-dragons.js
+++ aa04-class-dragons/all-dragons.js
@@ -1,12 +1,19 @@
-// Your code here 
+// Your code here
+const FriendlyDragon = require('./classes/friendly-dragon.js');
+const EvilDragon = require('./classes/evil-dragon.js');
+const Dragon = require('./classes/evil-dragon');
 
+let falkor = new FriendlyDragon("Falkor", "white", ["save Atreyu from the swamp", "save Atreyu from the Nothing", "scare the local bullies into a dumpster"], "Bastian");
+let smaug = new EvilDragon("Smaug", "black", ["take over your mountain kingdom", "steal all your dwarven gold", "burn down your floating village"], "Dwarf King");
 
+let allDragons = Dragon.getDragons(falkor, smaug);
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
   module.exports.falkor = falkor
   module.exports.smaug = smaug
+  module.exports.allDragons = allDragons;
 } catch {
   module.exports = null;
 }
--- aa04-class-dragons/classes/dragon.js
+++ aa04-class-dragons/classes/dragon.js
@@ -1,4 +1,24 @@
-// Your code here 
+// Your code here
+
+class Dragon {
+  constructor(name, color){
+    this.name = name;
+    this.color = color;
+  }
+
+  static getDragons(...Dragon){
+    return Dragon.map((dragon) => dragon.name);
+  }
+
+  breathesFire(){
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+}
+
+exports.modules = {
+  Dragon
+}
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
